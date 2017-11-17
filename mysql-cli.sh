5788c7b793b889dbc9b0bec5b6d3f27ceb021c3b
Chris Richardson <chris@chrisrichardson.net>
2017-11-17 10:50:51 -0800

    Fixed network name

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
 docker run $*  \
-   --network eventuatetramexamplesbasic_default \
+   --network eventuatetramcoreexamplesbasic_default \
    --name mysqlterm  --rm mysql:5.7.13 \
    sh -c 'exec mysql -hmysql -P3306 -uroot -prootpassword eventuate'
