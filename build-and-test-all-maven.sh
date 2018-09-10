934aadda7841dcca761e4101f159d6846668d77c
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-09-10 18:07:06 +0300

    Updating Spring Boot. Small fixes.

--- build-and-test-all-maven.sh
+++ build-and-test-all-maven.sh
@@ -4,7 +4,9 @@ set -e
 
 . ./set-env.sh
 
-mvn -f eventuate-tram-examples-in-memory/pom.xml clean install
+mvn compile
+
+mvn -f eventuate-tram-examples-in-memory/pom.xml install
 
 docker-compose down -v
 
@@ -12,6 +14,6 @@ docker-compose up -d
 
 ./wait-for-mysql.sh
 
-mvn clean install
+mvn install
 
 docker-compose down -v
