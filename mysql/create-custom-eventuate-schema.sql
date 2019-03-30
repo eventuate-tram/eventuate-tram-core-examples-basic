3f617f6d14ec2c70503280b0e9823e02973bc38f
Chris Richardson <chris@chrisrichardson.net>
2019-03-30 10:10:41 -0700

    Upgraded to Eventuate Tram 0.20.0.RC1

--- mysql/create-custom-eventuate-schema.sql
+++ mysql/create-custom-eventuate-schema.sql
@@ -3,23 +3,21 @@ GRANT ALL PRIVILEGES ON custom_eventuate_schema.* TO 'mysqluser'@'%' WITH GRANT 
 
 USE custom_eventuate_schema;
 
-DROP Table IF Exists message;
-DROP Table IF Exists received_messages;
-DROP Table IF Exists aggregate_instance_subscriptions;
-DROP Table IF Exists saga_instance;
-
 CREATE TABLE message (
-  id VARCHAR(1000) PRIMARY KEY,
+  id VARCHAR(767) PRIMARY KEY,
   destination VARCHAR(1000) NOT NULL,
   headers VARCHAR(1000) NOT NULL,
   payload VARCHAR(1000) NOT NULL,
-  published SMALLINT DEFAULT 0
+  published SMALLINT DEFAULT 0,
+  creation_time BIGINT
 );
 
 CREATE INDEX message_published_idx ON message(published, id);
 
 CREATE TABLE received_messages (
-  consumer_id VARCHAR(1000),
-  message_id VARCHAR(1000),
-  PRIMARY KEY(consumer_id, message_id)
+  consumer_id VARCHAR(767),
+  message_id VARCHAR(767),
+  PRIMARY KEY(consumer_id, message_id),
+  creation_time BIGINT
 );
+
