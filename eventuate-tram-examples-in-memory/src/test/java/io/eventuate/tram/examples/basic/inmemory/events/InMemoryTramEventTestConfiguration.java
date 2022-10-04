66aa774336d4473b610289036bb614738c087e6c
Chris Richardson <chris@chrisrichardson.net>
2022-10-04 12:53:03 -0700

    Fixed issues with Maven build by using spring-boot-starter-parent

--- eventuate-tram-examples-in-memory/src/test/java/io/eventuate/tram/examples/basic/inmemory/events/InMemoryTramEventTestConfiguration.java
+++ eventuate-tram-examples-in-memory/src/test/java/io/eventuate/tram/examples/basic/inmemory/events/InMemoryTramEventTestConfiguration.java
@@ -1,6 +1,5 @@
 package io.eventuate.tram.examples.basic.inmemory.events;
 
-import io.eventuate.common.spring.jdbc.EventuateTransactionTemplateConfiguration;
 import io.eventuate.tram.examples.basic.events.AbstractTramEventTestConfiguration;
 import io.eventuate.tram.spring.inmemory.TramInMemoryConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -11,8 +10,7 @@ import org.springframework.context.annotation.Import;
 @EnableAutoConfiguration
 @Import({
         AbstractTramEventTestConfiguration.class,
-        TramInMemoryConfiguration.class,
-        EventuateTransactionTemplateConfiguration.class
+        TramInMemoryConfiguration.class
 })
 class JdbcKafkaTramEventTestConfiguration {
 }
