936f07ea65bb00919130690a48172b2253116291
Chris Richardson <chris@chrisrichardson.net>
2023-12-18 11:35:16 +0900

    Fixed CircleCI configuration

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -3,7 +3,7 @@
 cat > generated_config.yml <<END
 version: 2.1
 orbs:
-  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.2.7"
+  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.2.8"
 workflows:
   version: 2.1
   build-test-and-deploy:
@@ -11,9 +11,13 @@ workflows:
 END
 
 for build_script in build-and-test-all*.sh ; do
+build_script_name=$(echo $build_script | sed -e 's/\.sh//' -e 's/build-and-test-all-//')
 cat >> generated_config.yml <<END
       - eventuate-gradle-build-and-test/build-and-test:
-          name: $build_script
+          name: $build_script_name
           script: ./$build_script
+          machine_image: ubuntu-2204:2023.07.2
+          java_version_to_install: '17'
+
 END
 done
