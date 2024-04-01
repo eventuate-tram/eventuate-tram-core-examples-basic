c01af3140ec7a29d9522212239238812f288c811
Chris Richardson <chris@chrisrichardson.net>
2024-04-01 08:53:41 -0700

    Added eventuate-tram-spring-consumer-common required for TramNoopDuplicateMessageDetectorConfiguration + used runtimeOnly for eventuate-tram-spring-consumer-$messageBroker

--- compile-all.sh
+++ compile-all.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -e
 
 for messageBroker in kafka rabbitmq activemq ; do
+   echo ==================== $messageBroker ====================
   ./gradlew --parallel -P messageBroker=$messageBroker assemble testClasses
 done
